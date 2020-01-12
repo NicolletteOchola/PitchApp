91a979976db146fe15d87dc7aa2c0e05fa00271e
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-12 17:54:01 +0300

    Created the paths for the home and about pages

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,19 @@
-from flask import (render_template, url_for, flash,redirect, request, abort, Blueprint)
-from flask_login import current_user, login_required
-from app import db
-from app.models import Post, User, Comment
-from app.posts.forms import PostForm, CommentForm
+from flask import render_template, request, Blueprint
+from app.models import Post
 
-posts = Blueprint('posts', __name__)
 
-@posts.route("/post/new", methods=['GET', 'POST'])
-@login_required
+main = Blueprint('main', __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.posted_date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html', posts=posts)
+
+
+@main.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
