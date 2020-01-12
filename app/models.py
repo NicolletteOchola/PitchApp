ffe491a5b4b5fc950ef0cc1514e1d7e64699cc65
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-12 23:28:58 +0300

    Added function add the new posts to the db

--- app/models.py
+++ app/models.py
@@ -20,3 +20,17 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f'User {self.username}'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    posted_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    category = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    comments = db.relationship('Comment', backref='post', lazy=True)
+
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.posted_date}', '{self.category}')"
+    
