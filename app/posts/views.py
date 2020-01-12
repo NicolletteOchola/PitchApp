7511fb33ff1f37734567345d97aed8203665f36c
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-12 22:35:25 +0300

    Added a function to validate that the current user is the post author

--- app/posts/views.py
+++ app/posts/views.py
@@ -24,3 +24,21 @@ def post(post_id):
     comments = Comment.query.filter_by(post_id = post_id).all()
     return render_template('post.html', title=post.title, post=post, comments = comments)
 
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your pitch has been updated!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('new-post.html', title='Update Post', form=form, legend='Update Post')
+
