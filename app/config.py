6b9075f83ee78c3f01a1906efc1df2cb7a0af689
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-14 16:55:39 +0300

    Deployment to heroku

--- app/config.py
+++ app/config.py
@@ -25,7 +25,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
@@ -35,7 +35,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://nicole:   @localhost/pitchapp'
     DEBUG = True
     
     
