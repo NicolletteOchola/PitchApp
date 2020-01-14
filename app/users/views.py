de0621c64bd646e9098d7c5c7b0182c6bff32e13
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-14 17:11:31 +0300

    commented out the python debugging code

--- app/users/views.py
+++ app/users/views.py
@@ -62,10 +62,10 @@ def register():
 @users.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.home'))
